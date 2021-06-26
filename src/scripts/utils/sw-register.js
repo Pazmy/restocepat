ae8a99c67b707a7737464f316ded509214dbda56
Pazmy <fadhillah17azmy01@gmail.com>
2021-06-26 23:24:03 +0700

    minor fixed

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,8 +1,6 @@
-import runtime from 'serviceworker-webpack-plugin/lib/runtime'
-
 const swRegister = async () => {
   if ('serviceWorker' in navigator) {
-    await runtime.register()
+    navigator.serviceWorker.register('./sw.js')
     return
   }
   console.log('Service worker not supported in this browser')
